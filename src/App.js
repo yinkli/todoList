9c3682dd3f5ce34f3169275aae636a5a15d048cf
Li Xianyang <yinkli@163.com>
2022-04-04 09:21:24 +0800

    todolist

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+var myFlag = false;
+
+
+
+class ListItem extends React.Component{
+  myDelete(name){
+    this.props.delete(name);
+  }
+  complete(name){
+    this.props.complete(name);
+  }
+  render(){
+    return (
+      <ul>
+      {
+        this.props.data.map(element => {
+          return (
+            <div>
+            <li className="listItem" key={element.name}  style={{background: element.isCompleted === false ? 'none' : '#FFEBCD'}}>
+              
+              <span>{element.name}</span>
+              <button className='complete' onClick={this.complete.bind(this, element.name)}>v</button>
+              <button className="delete"   onClick={this.myDelete.bind(this, element.name)}>x</button>
+              
+            </li>
+            <li className="fengexian">&nbsp;</li>
+            </div>
+            )
+      })
+    }
+    </ul>       
+    );
+  }
 }
 
-export default App;
+export default ListItem
--- src/index.js
+++ src/index.js
@@ -1,17 +1,111 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ListItem from './App';
+
+var myFlag = true;
+var myDebug = false;
+
+
+class ToDoList extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      list : [
+        {name : 'Task 1', isCompleted : true },
+        {name : 'Task 2', isCompleted : true },
+        {name : 'Task 3', isCompleted : false},
+        {name : 'Task 4', isCompleted : false},
+        {name : 'Task 5', isCompleted : false}
+      ],
+      input : ''
+    }
+  }
+
+  addItem(){
+    if(this.state.input === ''){
+      if(myDebug) alert(this.state.list.length);
+      return 
+    } else {
+      if(myDebug) alert('come in else');
+      const oldList = this.state.list;
+      if(myDebug) alert("oldList succeed");
+      const newItem = {name : this.state.input, isCompleted: false};
+      if(myDebug) alert("newList succeed");
+      oldList.push(newItem);
+      const newList = oldList
+      if(myDebug) alert("push succeed");
+      this.setState({
+          list: newList,
+          input: ''
+      });
+      if(myDebug) alert(newList);
+      if(myDebug) alert(this.state.list.length);
+    }
+  }
+
+  delete(name){
+    const oldList = this.state.list;
+    const newList = oldList.filter(
+      (value)=>{
+        if(value.name !== name){
+          return true;
+        } else {
+          return false;
+        }
+      }
+    );
+    this.setState({
+      list: newList,
+//        input: ''
+    });
+  }
+
+  complete(name){
+    const oldList = this.state.list;
+    const newList = oldList.map(
+      (value)=>{
+        if(value.name !== name){
+          return value;
+        } else {
+          const oldValue = value;
+          const newValue = oldValue;
+          newValue.isCompleted === false ? newValue.isCompleted = true : newValue.isCompleted = false;
+          return newValue
+        }
+      }
+    )
+    this.setState({
+      list: newList,
+//        input: ''
+    });
+  }
+
+  handleChange(e){
+    this.setState({
+      input: e.target.value
+    })      
+  }
+
+  render(){
+    return (
+      <div className="ToDoList">
+        <header>
+          <input type={"text"} value={this.state.input} onChange={this.handleChange.bind(this)} placeholder="Add task"></input>
+          <button className = "add item" onClick={this.addItem.bind(this)}>Add</button>
+        </header>
+        <footer>
+          <ListItem data = {this.state.list} delete = {this.delete.bind(this)} complete = {this.complete.bind(this)}/>
+        </footer>
+      </div>
+    );
+  }
+
+
+}
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <ToDoList />,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
